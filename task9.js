7fc6671a7a104413b7b2acd58d3cb50774e6074c
default23 ^^ <ivanov@taxisaturn.ru>
2016-11-12 19:21:16 +0300

    some problems

--- task9.js
+++ task9.js
@@ -4,4 +4,27 @@
  * Найдите сумму всех простых чисел меньше двух миллионов.
  */
 
+var maxSimple = 2000000;
+var currSimple = 2;
+var result = 0;
 
+while (currSimple < maxSimple) {
+
+	var isSimple = true;
+
+	for (var i = 2; i < 100; i++) {
+
+		if (i >= currSimple) break;
+
+		if (currSimple%i == 0) {
+			
+			isSimple = false;
+			break;
+		}
+	}
+
+	if (isSimple) result += currSimple;
+
+	currSimple++;
+}
+console.log("Max summ of simple numbers, less than " + maxSimple + " is : " + result);
